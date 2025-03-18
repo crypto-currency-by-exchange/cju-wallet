1ffe7163610c4f6e5051671b2ec2d694b7470a0e
cyberprophet <cyberprophet@hanmail.net>
2025-03-18 22:55:44 +0900

    first commit

---
+++ src/views/main_views.py
@@ -0,0 +1,15 @@
+from flask import (
+    Blueprint,
+    render_template,
+)
+
+from src.models import User
+
+bp = Blueprint("main", __name__, url_prefix="/")
+
+
+@bp.route("/", methods=["GET"])
+def home():
+    """메인 화면"""
+
+    return render_template("index.html")
